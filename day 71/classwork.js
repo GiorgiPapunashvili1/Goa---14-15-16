dc4042c881ce52a8201f368bc7b564fe64e51308
GiorgiPapunashvili1 <157594973+GiorgiPapunashvili1@users.noreply.github.com>
2024-10-21 13:05:28 +0400

    Seventy First commit

---
+++ day 71/classwork.js
@@ -0,0 +1,10 @@
+// const nums = [1, 2];
+
+// const {num1, num} = nums
+
+// console.log(nums)
+
+
+const person = { name: 'Alice', age: 25, city: 'New York' };
+
+console.log({name, age} = person)
