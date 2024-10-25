83ca5ed6e1012db855e184865644cfdbfbb16d50
GiorgiPapunashvili1 <157594973+GiorgiPapunashvili1@users.noreply.github.com>
2024-10-25 14:56:38 +0400

    day seventy third

---
+++ day 73/homework.js
@@ -0,0 +1,60 @@
+class animal {
+    constructor(name, species) {
+        this.name = name;
+        this.species = species;
+    }
+
+    makeSound() {
+        return "some sound";
+    }
+
+}
+
+class dog extends animal {
+    makeSound() {
+        return "bark";
+    }
+}
+
+class cat extends animal {
+    makeSound() {
+        return "meow";
+    }
+}
+
+class bird extends animal {
+    makeSound() {
+        return "tweet";
+    }
+}
+
+class vehicle {
+    constructor(brand, model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    startEngine() {
+        return "engine started";
+    }
+}
+
+class car extends vehicle {
+    startEngine() {
+        return "car engine started";
+    }
+}
+
+class truck extends vehicle {
+    startEngine() {
+        return "truck engine started";
+    }
+}
+
+class motorcycle extends vehicle {
+    startEngine() {
+        return "motorcycle"
+    }
+}
+
+console(vehicle.startEngine())
