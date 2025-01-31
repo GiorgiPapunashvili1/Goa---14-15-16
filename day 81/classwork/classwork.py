a86f46c947c855ab92fdab172a08cc0e25275af4
GiorgiPapunashvili1 <157594973+GiorgiPapunashvili1@users.noreply.github.com>
2025-01-31 19:03:06 +0400

    Eighty First commit

--- day 81/classwork/classwork.py
+++ day 81/classwork/classwork.py
@@ -1,1 +1,8 @@
-#we had code wars
+#8 kyu:
+def to_alternating_case(string):
+    return string.swapcase()
+
+
+#7 kyu
+def smaller(arr):
+    return [sum(b < a for b in arr[i + 1:]) for i, a in enumerate(arr)]
