dcac4d6279d21d2a96e47f813ac36d8c5f0b6150
GiorgiPapunashvili1 <157594973+GiorgiPapunashvili1@users.noreply.github.com>
2025-01-31 18:52:11 +0400

    Seventy Fifth commit

--- day 75/classwork.js
+++ day 75/classwork.js
@@ -1,1 +1,53 @@
-//no classwork
+// class Rectangle {
+//   constructor(widht, height) {
+//     this.width = widht;
+//     this.height = height;
+//   }
+
+//   getPerimeter() {
+//     return 2 * (this.width + this.height);
+//   }
+// }
+
+// const rect = new Rectangle(5, 10);
+// console.log("Perimeter:", rect.getPerimeter());
+
+
+
+
+
+// class Animal {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+
+//   speak() {
+//     console.log(`${this.name} makes a sound.`);
+//   }
+// }
+
+// class Mammal extends Animal {
+//   constructor(name, age, furColor) {
+//     super(name, age);
+//     this.furColor = furColor;
+//   }
+
+//   speak() {
+//     console.log(`${this.name} makes a mammal sound.`);
+//   }
+// }
+
+// class Dog extends Mammal {
+//   constructor(name, age, furColor, breed) {
+//     super(name, age, furColor);
+//     this.breed = breed;
+//   }
+
+//   speak() {
+//     console.log(`${this.name} barks.`);
+//   }
+// }
+
+// const myDog = new Dog("Buddy", 3, "Brown", "Golden Retriever");
+// myDog.speak();
